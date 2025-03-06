dea32af89930929a7d9e1777e8a67ca10e6c323e
ruanquanyuan <ruanquanyuan@outlook.com>
2025-03-06 18:53:02 +0800

    add resource

--- static/js/index.js
+++ static/js/index.js
@@ -57,22 +57,4 @@ $(document).ready(function() {
     		console.log(state);
     	});
     }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
-    bulmaSlider.attach();
-
 })
